4f78932088e007bceae4705d8fed36ace05fa3e1
Dan Blanchette <dlchetter@gmail.com>
2025-05-21 13:54:13 -0700

    Update main.py

--- pythonExamples/main.py
+++ pythonExamples/main.py
@@ -1,18 +1,24 @@
-import random
+import random as rand
 
 # proper set up of a main function in Python
 
 def printMessages(message1, messsage2):
    print(message1, "is", messsage2)
 
+def printRandomMessage(message1, message2):
+   print(message1, "is", rand.choice(message2))
+
 def main():
-   print("Hello World!")
+   # print("Hello World!")
    message1 = "Coding"
-   message2 = ["fun", "hard", "easy", "interesting"]
-   printMessages(message1, message2[0])
-   printMessages(message1, message2[1])
-   printMessages(message1, message2[2])
-   printMessages(message1, message2[3])
+   message2 = ["fun", "hard", "easy", "interesting", "trivial", "dumb", "binary"]
+   # printMessages(message1, message2[0])
+   # printMessages(message1, message2[1])
+   # printMessages(message1, message2[2])
+   # printMessages(message1, message2[3])
+   
+   for i in range(10):
+      printRandomMessage(message1, message2)
 
 if __name__ == "__main__":
    main()
